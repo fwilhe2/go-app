ddc72f58c7b23410c73cde9cb782ae698d21ff58
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-05-23 09:08:41 +0200

    push

--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"io/ioutil"
 	"github.com/goccy/go-yaml"
 )
 
-func main() {
-	yml := `
-%YAML 1.2
----
-a: 1
-b: c
-`
-	var v struct {
-		A int
-		B string
+func noError(e error) {
+	if e != nil {
+		panic(e)
 	}
-	if err := yaml.Unmarshal([]byte(yml), &v); err != nil {
+}
+
+func main() {
+	dat, err := ioutil.ReadFile(".github/workflows/ci.yml")
+	noError(err)
+
+	type Step struct {
+		Name string
+		Run string
+		Uses string
+	}
+
+	type Job struct {
+		RunsOn string `yaml:"runs-on"`
+		Steps []Step `yaml:"steps"`
+	}
+
+	var v struct {
+		Name string
+		On map[string]interface{}
+		Jobs map[string] struct {
+			RunsOn string `yaml:"runs-on"`
+			Steps []Step `yaml:"steps"`
+		}
+	}
+	if err := yaml.Unmarshal(dat, &v); err != nil {
 		panic("foo")
 	}
-	println(&v)
+	println(v.Name)
+	println(v.On["push"])
+	println(v.Jobs["build"].RunsOn)
+	for i := 0; i < len(v.Jobs["build"].Steps); i++ {
+		println(v.Jobs["build"].Steps[i].Name)
+		println(v.Jobs["build"].Steps[i].Uses)
+		println(v.Jobs["build"].Steps[i].Run)
+	}
 }
