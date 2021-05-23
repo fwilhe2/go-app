d51e1e41925144475da7963b55d1c32fa655d22d
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-05-23 11:06:02 +0200

    push

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"io/ioutil"
 	"github.com/goccy/go-yaml"
+	"io/ioutil"
 )
 
 func noError(e error) {
@@ -17,32 +17,36 @@ func main() {
 
 	type Step struct {
 		Name string
-		Run string
+		Run  string
 		Uses string
 	}
 
 	type Job struct {
 		RunsOn string `yaml:"runs-on"`
-		Steps []Step `yaml:"steps"`
+		Steps  []Step `yaml:"steps"`
 	}
 
-	var v struct {
+	var workflow struct {
 		Name string
-		On map[string]interface{}
-		Jobs map[string] struct {
-			RunsOn string `yaml:"runs-on"`
-			Steps []Step `yaml:"steps"`
-		}
+		On   map[string]interface{}
+		Jobs map[string]Job
 	}
-	if err := yaml.Unmarshal(dat, &v); err != nil {
+	if err := yaml.Unmarshal(dat, &workflow); err != nil {
 		panic("foo")
 	}
-	println(v.Name)
-	println(v.On["push"])
-	println(v.Jobs["build"].RunsOn)
-	for i := 0; i < len(v.Jobs["build"].Steps); i++ {
-		println(v.Jobs["build"].Steps[i].Name)
-		println(v.Jobs["build"].Steps[i].Uses)
-		println(v.Jobs["build"].Steps[i].Run)
+	println(workflow.Name)
+	for trigger, _ := range workflow.On {
+		println("Workflow runs on trigger " + trigger)
+	}
+
+	for jobName, job := range workflow.Jobs {
+		println("Job: " + jobName)
+		println("Runs On: " + job.RunsOn)
+		for x := range job.Steps {
+			println("  Name: " + job.Steps[x].Name)
+			println("  Uses: " + job.Steps[x].Uses)
+			println("  Run:  " + job.Steps[x].Run)
+			println()
+		}
 	}
 }
